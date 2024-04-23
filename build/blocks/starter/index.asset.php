ff518a7aa3aae6f01f2d8b421ebd4be92dd53e88
Touhidul Sadeek <touhid.bd@live.com>
2024-04-23 20:24:47 +0600

    Fix issue for multiple blocks

--- build/blocks/starter/index.asset.php
+++ build/blocks/starter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '34b9a60fea1a182df5e1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '90fe8365676d8ac7fc7b');
